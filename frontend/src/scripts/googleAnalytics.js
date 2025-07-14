cbc617a6ba3ef7522e23709d18209b0e1b0110f5
Lux <luxetnoxesports@gmail.com>
2025-07-14 16:25:33 -0600

    google analytics test

---
+++ frontend/src/scripts/googleAnalytics.js
@@ -0,0 +1,13 @@
+(function () {
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-1Q3YSS4HPD";
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+  gtag('config', 'G-1Q3YSS4HPD');
+})();
