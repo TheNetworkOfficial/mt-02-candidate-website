282ebe53f2f79b5fa2367a70565d969f5ef62528
Lux <luxetnoxesports@gmail.com>
2025-07-14 16:28:47 -0600

    google analytics tag change

--- frontend/src/scripts/googleAnalytics.js
+++ frontend/src/scripts/googleAnalytics.js
@@ -1,7 +1,7 @@
 (function () {
   const script = document.createElement("script");
   script.async = true;
-  script.src = "https://www.googletagmanager.com/gtag/js?id=G-1Q3YSS4HPD";
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-78XPQCG150";
   document.head.appendChild(script);
 
   window.dataLayer = window.dataLayer || [];
@@ -9,5 +9,5 @@
     window.dataLayer.push(arguments);
   }
   gtag("js", new Date());
-  gtag('config', 'G-1Q3YSS4HPD');
+  gtag('config', 'G-78XPQCG150');
 })();
